1cb0fce8ec7f67906f1d9d9c25433551351458b5
Pedro Lucas <83101441+thorxenon@users.noreply.github.com>
2023-09-10 11:56:18 -0300

    feat: layout run out

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from 'tailwind-merge';
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+import { Button } from '.';
 
 interface HeaderProps{
     children: React.ReactNode,
@@ -81,7 +82,25 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
                     <BiSearch className="text-black" size={20}/>
                 </button>
             </div>
+            <div className="flex justify-between items-center gap-x-4">
+                <>
+                    <div>
+                        <Button
+                            className="bg-transparent text-neutral-300 font-medium"
+                            onClick={()=>{}}
+                        >Sign up</Button>
+                    </div>
+
+                    <div>
+                        <Button
+                            className="bg-white px-6 py-2"
+                            onClick={()=>{}}
+                        >Log in</Button>
+                    </div>
+                </>
+            </div>
         </div>
+        {children}
     </div>
   )
 }
--- components/index.ts
+++ components/index.ts
@@ -3,12 +3,15 @@ import Box from './Box';
 import SidebarItem from './SidebarItem';
 import Library from './Library';
 import Header from './Header';
-
+import Button from './Button';
+import ListItem from './ListItem';
 
 export {
     SideBar,
     Box,
     SidebarItem,
     Library,
-    Header
+    Header,
+    Button,
+    ListItem
 }
