e75d24afdeacb44cb3734cf359df4c3ab19bfff9
Pedro Lucas <83101441+thorxenon@users.noreply.github.com>
2023-09-11 20:48:15 -0300

    feat: upload songs and stuff

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 
 import AuthModal from '@/components/AuthModal';
+import UploadModal from '@/components/UploadModal';
 
 const ModalProvider = () => {
 
@@ -19,6 +20,7 @@ const ModalProvider = () => {
   return (
     <>
       <AuthModal/>
+      <UploadModal/>
     </>
   )
 }
